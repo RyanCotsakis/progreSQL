911c57ae3b927416fe00c837124a7a5dcaf6123e
Ryan Cotsakis <public@cotsakis.com>
2026-08-29 09:52:26 +0200

    Version 1.0.0

--- app/main.py
+++ app/main.py
@@ -32,7 +32,7 @@ from app.services import (
 st.set_page_config(page_title="ProgreSQL", page_icon="💪", layout="wide")
 
 AUTH_SESSION_TIMEOUT = timedelta(hours=1)
-APP_VERSION = "0.9"
+APP_VERSION = "1.0.0"
 
 
 def require_authorized_user() -> None:
